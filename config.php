62a2da5222d90cbb97debdf1b2abdcb4af55c6d7
Sisira Narayana <sisiran@amazon.com>
2021-11-23 23:42:25 -0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'ls-bb0cf7daf5cf95380191aefc263b532ed2a4ece0.cmuvblbnrbtw.us-west-2.rds.amazonaws.com');
-define('DB_USERNAME', 'sisiradb');
-define('DB_PASSWORD', 'sisiradb');
-define('DB_NAME', 'sisiradb');
+define('DB_SERVER', 'ls-2a84279bbd68730697affd6e2cc5b08bfb40864d.cmuvblbnrbtw.us-west-2.rds.amazonaws.com');
+define('DB_USERNAME', 'reinvent21');
+define('DB_PASSWORD', 'reinvent21');
+define('DB_NAME', 'reinvent21');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
